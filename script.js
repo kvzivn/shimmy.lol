0a425fb7bd6d85bafe6e34b7e8990bdc51396008
Kevin <hello@kevinivan.com>
2021-12-12 14:36:22 +0100

    updates

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 console.log("shimmy")
+
+$(document).ready(function () {
+  startTime()
+  $("#startMenu").hide()
+
+  $("#start").click(function () {
+    $("#startMenu").toggle()
+    $(this).toggleClass("startClick")
+  })
+
+  $("#desktop").click(function () {
+    console.log("click")
+    // $("#startMenu").toggle()
+    // $("#start").removeClass("startClick").addClass("startRest")
+  })
+
+  $(".desktopIcon").dblclick(function () {
+    alert($(this).text())
+  })
+})
+
+function startTime() {
+  var today = new Date()
+  var h = today.getHours()
+  var m = today.getMinutes()
+  var s = today.getSeconds()
+  // add a zero in front of numbers<10
+  m = checkTime(m)
+  s = checkTime(s)
+  var hd = h
+  $("#clock").html(
+    (hd = 0 ? "12" : hd > 12 ? hd - 12 : hd) +
+      ":" +
+      m +
+      " " +
+      (h < 12 ? "AM" : "PM")
+  )
+  t = setTimeout(function () {
+    startTime()
+  }, 500)
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  }
+  return i
+}
